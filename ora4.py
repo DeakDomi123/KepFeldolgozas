6f222f26e73fa11366ceb53f75fa735a397e9b5c
DeakDomi123 <deakdomi1024@gmail.com>
2023-09-25 19:11:21 +0200

    Update

--- ora4.py
+++ ora4.py
@@ -1,3 +1,17 @@
 import cv2
 import numpy as np
-import random
+#Feladat1 kép élesítése
+'''value = 3
+sigma_value = 2
+input = cv2.imread(".\\Kepek\\noisy-image.png")
+output = cv2.boxFilter(input, -1, (value, value))
+cv2.imshow('Kép', output)''' 
+#Feladat2 kép élesítése 
+image = cv2.imread(".\\Kepek\\noisy-image.png")
+blurred = cv2.GaussianBlur(image, (image.shape[0], image.shape[1]), 1)
+unsharped = cv2.addWeighted(image, 1.5, blurred, -0.5, 0)
+cv2.imshow("Original", image)
+cv2.imshow("Unsharped", unsharped)
+cv2.imshow("Blurred", blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
